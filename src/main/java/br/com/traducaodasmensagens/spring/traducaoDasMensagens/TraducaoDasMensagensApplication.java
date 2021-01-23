5182530a1295dcee4d872436e51a7e67f8fdc1f8
filiperdt <rubenfilipeao@gmail.com>
2021-01-23 16:53:34 -0200

    Commit inicial 2

---
+++ src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/TraducaoDasMensagensApplication.java
@@ -0,0 +1,13 @@
+package br.com.traducaodasmensagens.spring.traducaoDasMensagens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraducaoDasMensagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraducaoDasMensagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/TraducaoDasMensagensApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.traducaodasmensagens.spring.traducaoDasMensagens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraducaoDasMensagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
