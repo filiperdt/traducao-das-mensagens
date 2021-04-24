86d93437e1c0f572ee05223d00da1ba1fc66467e
filiperdt <rubenfilipeao@gmail.com>
2021-04-24 15:41:11 -0300

    Criados fragmentos, e alteradas as rotas para index e home.

--- src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/LoginController.java
+++ src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/LoginController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class IndexController {
+public class LoginController {
 	
-	@GetMapping("/")
-	public ModelAndView index() {
-		ModelAndView mv = new ModelAndView("index");
+	@GetMapping("/mensagens/login")
+	public ModelAndView login() {
+		ModelAndView mv = new ModelAndView("login");
 		return mv;
 	}
 }
