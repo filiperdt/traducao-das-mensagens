ce671d87954f496dc383bd4a94ff267938d5709e
filiperdt <rubenfilipeao@gmail.com>
2021-05-12 15:03:04 -0300

    Editadas as páginas de Login e de Listagem de Usuários

--- src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/ParagrafoController.java
+++ src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/ParagrafoController.java
@@ -1,5 +1,29 @@
 package br.com.traducaodasmensagens.spring.traducaoDasMensagens.controller;
 
-public class ParagrafoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.traducaodasmensagens.spring.traducaoDasMensagens.orm.Paragrafo;
+import br.com.traducaodasmensagens.spring.traducaoDasMensagens.repository.ParagrafoRepository;
+
+@Controller
+@RequestMapping("/paragrafos")
+public class ParagrafoController {
+	@Autowired
+	private ParagrafoRepository paragrafoRepository;
+	
+	@GetMapping("")
+	public ModelAndView index() {
+		List<Paragrafo> paragrafos = paragrafoRepository.findAll();
+		
+		ModelAndView mv = new ModelAndView("paragrafos/paragrafos");
+		mv.addObject("paragrafos", paragrafos);
+		
+		return mv;
+	}
 }
--- src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/UsuarioController.java
+++ src/main/java/br/com/traducaodasmensagens/spring/traducaoDasMensagens/controller/UsuarioController.java
@@ -1,5 +1,63 @@
 package br.com.traducaodasmensagens.spring.traducaoDasMensagens.controller;
 
-public class UsuarioController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.traducaodasmensagens.spring.traducaoDasMensagens.orm.Usuario;
+import br.com.traducaodasmensagens.spring.traducaoDasMensagens.repository.UsuarioRepository;
+
+@Controller
+@RequestMapping("/usuarios")
+public class UsuarioController {
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@GetMapping("")
+	public ModelAndView index() {
+		List<Usuario> usuarios = usuarioRepository.findAll();
+		
+		ModelAndView mv = new ModelAndView("usuarios/usuarios");
+		mv.addObject("usuarios", usuarios);
+		
+		return mv;
+	}
+	
+	@GetMapping("/new")
+	public ModelAndView nnew() {
+		ModelAndView mv = new ModelAndView("usuarios/novo");
+		
+		return mv;
+	}
+	
+	@PostMapping("")
+	public ModelAndView create() {
+		return null;
+	}
+	
+	@GetMapping("/{id}")
+	public ModelAndView show(@PathVariable Integer id) {
+		return null;
+	}
+	
+	@GetMapping("/{id}/edit")
+	public ModelAndView edit(@PathVariable Integer id) {
+		return null;
+	}
+	
+	@PostMapping("/{id}")
+	public ModelAndView update(@PathVariable Integer id) {
+		return null;
+	}
+	
+	@GetMapping("/{id}/delete")
+	public ModelAndView delete(@PathVariable Integer id) {
+		return null;
+	}
 }
